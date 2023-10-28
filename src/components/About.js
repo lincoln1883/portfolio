920c02017ecb2443acd833a96e5718cd919865b4
Lincoln Gibson <lincolngibson7@gmail.com>
2023-10-28 18:08:13 -0300

    Update title.

--- src/components/About.js
+++ src/components/About.js
@@ -9,7 +9,7 @@ const About = () => {
 				<div className="lg: flex-grow md:w-1.5 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
 					<h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white">
 						Hi, I'm Lincoln
-						<br className=" hidden lg:inline-block" /> Web Developer.
+						<br className=" hidden lg:inline-block" /> Fullstack Developer.
 					</h1>
 					<p className="mb-8 leading-relaxed">
 						I'm a self-taught web developer eager to build solutions for
