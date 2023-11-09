aa33d0348f6e0e6102bec1b4f5c988e8f3f77f14
Lincoln Gibson <lincolngibson7@gmail.com>
2023-11-08 21:25:30 -0300

    Refactor data.js file with updated info.

--- src/data.js
+++ src/data.js
@@ -1,45 +1,145 @@
 export const projects = [
 	{
-		title: "React Youtube Clone",
-		subtitle: "React & Material UI",
+		title: 'Handyhome Hub',
+		subTitle: 'Handyhome Hub',
 		description:
-			"This was a general idea project, built using React and styled with Matreial-UI for interacting with RapidAPI endpoints.  ",
-		image: "./project-1.png",
-		link: "https://phenomenal-tarsier-b1b00b.netlify.app",
+			'This application helps homeowners to connect with specialized home service providers. It provides a platform for homeowners to find and hire professionals for various home services. Build as an API capstone project for the Microverse program, it uses the Ruby on Rails framework to build the backend API and React and Redux for the frontend.',
+		image: './capstoneDT.png',
+		technologies: ['React', 'Redux', 'Tailwind', 'React-Router', 'Jest', 'Axios'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://handyhomehub.netlify.app/',
+		source: 'https://github.com/lincoln1883/appointment-capstone-frontend',
 	},
 	{
-		title: "Reddit Client",
-		subtitle: "React & Redux",
+		title: 'Popcorn-flix',
+		subTitle: 'Popcorn-flix',
 		description:
-			"Reddit client project was done as apart of the frontend development course for Codecademy demonstrating knowledge of state management using Redux to make HTTP request, built using React-Redux and the reddit api",
-		image: "./project-2.png",
-		link: "https://illustrious-chaja-ec35b5.netlify.app",
+			'The Popcorn Flix App is the Capstone project for React module three where we are required to build an SPA utilizing the concepts we learnt. I used Axios to make calls to a movies API to fetch data, I used Redux( useSelector and useDispatch) to update the state by filtering movies by title. I can navigate from the home page to a single movie page',
+		image: './netflix.png',
+		technologies: ['React', 'Redux', 'Tailwind', 'Jest', 'React Router', 'Axios'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://popcorn-flix.netlify.app/',
+		source: 'https://www.github.com/lincoln1883/net-flix-capstone',
 	},
 	{
-		title: "Handywork",
-		subtitle: "React,Redux,Nodejs,Jest,Express,Postgres,Tailwind  ",
+		title: 'Space Xplorers',
+		subTitle: 'Space Xplorers',
 		description:
-			"This  project was built was done as part of aChingu voyage project which taught us how software is developed using the agile scrum methodology. The technologies we used include React, Redux,Tailwind,Jest, Nodejs, Express and Postgres.",
-		image: "./project-3.png",
-		link: "https://handywork-clients.netlify.app",
+			'The Space X app is a React project where we build a Group Capstone Project. It uses Axios to fetch data from rockets and missions API endpoints. Redux (useSelector and useDispatch) handles state updates for mission and rocket reservations and cancellations. React router enables page navigation, while useEffect fetches data.',
+		image: './space-x.png',
+		technologies: ['React', 'Redux', 'Bootstrap', 'React Router', 'Axios'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://spacexplorers.netlify.app/',
+		source: 'https://github.com/lincoln1883/space-x',
 	},
 	{
-		title: "Cafe ",
-		subtitle: "HTML and CSS",
+		title: 'Anancy Bookstore',
+		subTitle: 'Anancy Bookstore',
 		description:
-			"I contributed to the building of this website as apart of the intended humanitarian efforts by owners,who wanted to assist Ukranian refugees with breakfast and lunch. ",
-		image: "./project-4.png",
-		link: "https://thusbanduk.github.io/CafeKalyna",
+				'The Anancy Bookstore app is a project assignment of module three where we are required to build our second React app. It is a simple bookstore where you can add and delete books. features include React Hooks,React Router, App wide state Management with and Redux & Redux Toolkit and Api calls with Axios',
+		image: './bookstore.png',
+		technologies: ['React', 'Redux', 'Tailwind CSS', 'Jest', 'React-Router'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://anancy-bookstore.netlify.app/',
+		source: 'https://github.com/lincoln1883/anancy-bookstore',
+	},
+	{
+		title: 'Math Magicians',
+		subTitle: 'Math Magicians',
+		description:
+			'The Math magicians app is a project assignment of module three where we are required to build our first React app. It is a simple math calculator where you can perform basic math operations.It features include React Hooks,React Router, Jest and React Testing Library.',
+		image: './math-magicians.png',
+		technologies: ['React', 'Css', 'Jest', 'React Router', 'Axios'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://math-magician-iq2v.onrender.com/',
+		source: 'https://www.github.com/lincoln1883/math-magicians',
+	},
+	{
+		title: 'Reggae Sumfest',
+		subTitle: 'Reggae Sumfest',
+		description:
+			'This project was a requirement of module one, where we learned HTML, CSS and JavaScript, also the importance of git workflows following best practices of committing early and often. The use of linters to ensure code quality follows the HTML and CSS best practices.',
+		image: './capstone1.png',
+		technologies: ['JavaScript', 'Css', 'Html', 'linters'],
+		skills: [
+			'Codekit',
+			'Github',
+			'Javascript',
+			'Bootstrap',
+			'Terminal',
+			'Codepen',
+		],
+		link: 'https://lincoln1883.github.io/mod1-capstone-project/',
+		source: 'https://www.github.com/lincoln1883/mod1-capstone-project',
 	},
 ];
 
 export const skills = [
-	"GIT",
-	"JAVASCRIPT",
-	"REACT",
-	"REDUX",
-	"NODE",
-	"EXPRESS",
-	"TAILWIND",
-	"POSTGRES",
+	{
+		title: "REACT",
+		icon: './react-2.svg'
+	},
+	{
+		title: "REDUX",
+		icon: './redux.svg'
+	},
+	{
+		title: "RAILS",
+		icon: './rails-1.svg'
+	},
+	{
+		title: "RUBY",
+		icon: './ruby.svg'
+	},
+	{
+		title: "GIT",
+		icon: './git-icon.svg'
+	},
+	{
+		title: "JAVASCRIPT",
+		icon: './logo-javascript.svg'
+	},
+	{
+		title: "TAILWIND",
+		icon: './tailwind-css-2.svg'
+	},
+	{
+		title: "POSTGRES",
+		icon: './postgresql.svg'
+	}
 ];
